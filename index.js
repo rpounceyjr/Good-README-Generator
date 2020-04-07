e291870f204ae8d9dceeeb1b28fceaf84c1afd2a
rpounceyjr <rpounceyjr@gmail.com>
2020-04-07 16:30:30 -0400

    added question for project name.  Project name now prints to README.

--- index.js
+++ index.js
@@ -2,33 +2,51 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
 
-const questions = ["What is your GitHub username?"
+const questions = ["What is your GitHub username?", "What is the name of this project?"
 ];
 
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("written!");
+        }
+    })
 }
 
 async function init() {
-    let userName = await
-        inquirer
+    // let userName = await
+       await inquirer
             .prompt([
                 {
                     type: "input",
                     message: questions[0],
                     name: "username"
+                },
+                {
+                    type: "input",
+                    message: questions[1],
+                    name: "project"
                 }
-            ]).then((response) => response.username);
-    
-        await axios.get(`https://api.github.com/users/${userName}`)
-        .then((response) => console.log(response.data));
-        
-        fs.appendFile("log.txt", userName, err => {
-            if(err){
-                console.log(err)
-            }else{
-                console.log("success!")
-            }
-        })
+
+            ]).then((response) => {
+                userName = response.username;
+                projectName = response.project;
+
+            });
+
+    await axios.get(`https://api.github.com/users/${userName}`)
+        .then((response) => {
+            const allInfo =
+                `#${projectName}\n
+            by: ${response.data.name} \n
+            ![Roger Pouncey picture](${response.data.avatar_url})`
+
+            writeToFile("log.md", allInfo);
+        });
+
+
 }
 
 init();
---
+++ log.md
@@ -0,0 +1,5 @@
+#something
+
+            by: Roger Pouncey 
+
+            ![Roger Pouncey picture](https://avatars2.githubusercontent.com/u/58075467?v=4)
