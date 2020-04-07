63c0cc9e9912ed5478ea32df0cc76cf824cefff1
rpounceyjr <rpounceyjr@gmail.com>
2020-04-07 14:48:30 -0400

    added assignment instructions for reference.  Got async/await to get github user name and retrieve user info from github using axios.

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const axios = require("axios");
 
 const questions = ["What is your GitHub username?"
-
 ];
 
 function writeToFile(fileName, data) {
 }
 
-function init() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: questions[0],
-                name: "username"
-            }
-        ]
-        )
+async function init() {
+    let userName = await
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: questions[0],
+                    name: "username"
+                }
+            ]).then((response) => response.username);
+    
+        axios.get(`https://api.github.com/users/${userName}`)
+        .then((response) => console.log(response.data));
+        
 
 }
 
